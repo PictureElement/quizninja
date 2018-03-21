8a79de18c1d9dc8235feff53d9f18f41740483d9
Marios Sofokleous <marios.sofokleous@yandex.com>
2018-03-21 14:32:17 +0200

    Add card 3 (true-false)

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,44 @@
-// Object
-var quiz = new Object();
-// Array of objects
-var questions = new Array();
+// Quiz ctor
+function Quiz(questions, id, title, description) {
+  this.questions = questions;
+  this.id = id;
+  this.title = title;
+  this.description = description;
+  this.currentQuestionIndex = 0;
+  this.score = 0;
+}
 
-// Run once the DOM is ready
+function populate() {
+  var 
+}
+
 $(document).ready(function() {
-  // Perform an AJAX (asynchronous HTTP) request
-  $.ajax({
-    method: "GET",
-    url: "https://proto.io/en/jobs/candidate-questions/quiz.json",
-    dataType: "json",
-    // Disallow use of cached results
-    cache: "false",
-    // Callback option is invoked, if the request fails.
-    //error
-    // Callback option is invoked, if the request succeeds.
-    success: function(data) {
-      // Initialize 'quiz' object
-      quiz.id = data.quiz_id;
-      quiz.title = data.title;
-      quiz.description = data.description;
-      // Initialize 'questions' array
-      $(data.questions).each(function(index, value) {
-        questions.push(value);
+    $("#play-btn").click(function() {
+      $.ajax({
+        method: "GET",
+        url: "https://proto.io/en/jobs/candidate-questions/quiz.json",
+        dataType: "json",
+        // Disallow use of cached results
+        cache: "false",
+        // Callback option is invoked, if the request fails.
+        //error
+        // Callback option is invoked, if the request succeeds.
+        success: function(data) {
+          quizData = data;    
+        }
       });
-    }
-  });
+    });
 });
 
-console.log(quiz);
-console.log(questions);
+var id = quizData.quiz_id;
+var title = quizData.title;
+var description = quizData.description;
+var questions = new Array();
+// Initialize 'questions' array
+$(quizData.questions).each(function(index, value) {
+  questions.push(value);
+});
+
+// Create quiz
+var quiz = new Quiz(questions, id, title, description);
+
