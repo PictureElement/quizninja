14dbe2bc3207c164af94cbd62eb2857992d89b14
Marios Sofokleous <marios.sofokleous@yandex.com>
2018-03-22 16:27:02 +0200

    Add HTML strings for multiple choice(single) buttons

--- src/js/helper.js
+++ src/js/helper.js
@@ -2,10 +2,14 @@
 // placeholder text you see in them.
 
 // Common
-var HTMLcardImage = "<img class='img-fluid rounded' src='%data%' alt='placeholder'>";
-var HTMLquestion = "<p class='p-3 m-0 text-center'>%data%</p>";
+var HTMLcardImage = '<img class="img-fluid rounded" src="%data%" alt="placeholder">';
+var HTMLquestion = '<p class="p-3 m-0 text-center">%data%</p>';
 
 // Multiple choice (single)
+var HTMLmultipleChoiceSingleButton1 = '<input type="radio" autocomplete="off" name="options" id="option1" value="1"> %data%';
+var HTMLmultipleChoiceSingleButton2 = '<input type="radio" autocomplete="off" name="options" id="option2" value="2"> %data%';
+var HTMLmultipleChoiceSingleButton3 = '<input type="radio" autocomplete="off" name="options" id="option3" value="3"> %data%';
+var HTMLmultipleChoiceSingleButton4 = '<input type="radio" autocomplete="off" name="options" id="option4" value="4"> %data%';
 
 // Multiple choice (multiple)
 
