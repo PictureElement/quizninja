3a7f7e29ac3cbbbf6ef0fad0551f48e84dc56d1d
Marios Sofokleous <marios.sofokleous@yandex.com>
2018-03-22 21:02:32 +0200

    Show possible answers in populate()

--- src/js/helper.js
+++ src/js/helper.js
@@ -6,17 +6,11 @@ var HTMLimage = '<img class="img-fluid rounded" src="%data%" alt="placeholder">'
 var HTMLquestion = '<p class="p-3 m-0 text-center">%data%</p>';
 
 // Multiple choice (single)
-var HTMLradio1 = '<label class="mb-2 btn btn-sm btn-block btn-primary"><input type="radio" autocomplete="off" name="options" id="option1" value="1"> %data%</label>';
-var HTMLradio2 = '<label class="mb-2 btn btn-sm btn-block btn-primary"><input type="radio" autocomplete="off" name="options" id="option2" value="2"> %data%</label>';
-var HTMLradio3 = '<label class="mb-2 btn btn-sm btn-block btn-primary"><input type="radio" autocomplete="off" name="options" id="option3" value="3"> %data%</label>';
-var HTMLradio4 = '<label class="mb-2 btn btn-sm btn-block btn-primary"><input type="radio" autocomplete="off" name="options" id="option4" value="4"> %data%</label>';
+var HTMLradio = '<label class="mb-2 btn btn-sm btn-block btn-primary"><input type="radio" autocomplete="off" name="options" id="option%data1%" value="%data1%"> %data2%</label>';
 
 // Multiple choice (multiple)
-var HTMLcheckbox1 = '<label class="mb-2 btn btn-sm btn-block btn-primary"><input type="checkbox" autocomplete="off" name="options" id="option1" value="1"> %data%</label>';
-var HTMLcheckbox2 = '<label class="mb-2 btn btn-sm btn-block btn-primary"><input type="checkbox" autocomplete="off" name="options" id="option2" value="2"> %data%</label>';
-var HTMLcheckbox3 = '<label class="mb-2 btn btn-sm btn-block btn-primary"><input type="checkbox" autocomplete="off" name="options" id="option3" value="3"> %data%</label>';
-var HTMLcheckbox4 = '<label class="mb-2 btn btn-sm btn-block btn-primary"><input type="checkbox" autocomplete="off" name="options" id="option4" value="4"> %data%</label>';
+var HTMLcheckbox = '<label class="mb-2 btn btn-sm btn-block btn-primary"><input type="checkbox" autocomplete="off" name="options" id="option%data1%" value="%data1%"> %data2%</label>';
 
 // True-false
-var HTMLfalse = '<label class="mb-2 btn btn-sm btn-block btn-primary"><input type="radio" autocomplete="off" name="options" id="false" value="0"> %data%</label>';
-var HTMLtrue = '<label class="mb-2 btn btn-sm btn-block btn-primary"><input type="radio" autocomplete="off" name="options" id="true" value="1"> %data%</label>';
+var HTMLfalse = '<label class="mb-2 btn btn-sm btn-block btn-primary"><input type="radio" autocomplete="off" name="options" id="false" value="0"> False</label>';
+var HTMLtrue = '<label class="mb-2 btn btn-sm btn-block btn-primary"><input type="radio" autocomplete="off" name="options" id="true" value="1"> True</label>';
--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,10 @@ function populate(quiz) {
   
   var score = 0;
   var currentQuestionIndex = 0;
-  var 
   
+  // Get question type
+  var questionType = quiz.questions[currentQuestionIndex].question_type;
+
   // Show image  
   var formattedImage = HTMLimage.replace("%data%", quiz.questions[currentQuestionIndex].img); // Replace the %data% placeholder text
   $("#card-image").append(formattedImage); // Append image
@@ -14,10 +16,28 @@ function populate(quiz) {
   $("#card-body").prepend(formattedQuestion);
 
   // Show possible answers
-  for (var i = 0; i < quiz.questions[currentQuestionIndex].possible_answers.length; i++) {
-    
+  if (questionType === "mutiplechoice-single") { // For multiple choice (single) use radio buttons
+    var formattedRadio;
+    // Show possible answers
+    for (var i = 0; i < quiz.questions[currentQuestionIndex].possible_answers.length; i++) {
+      formattedRadio = HTMLradio.replace("%data1%", i+1);
+      formattedRadio = formattedRadio.replace("%data2%", quiz.questions[currentQuestionIndex].possible_answers[i].caption);
+      $("#card-answers").append(formattedRadio);
+    }
   }
-
+  else if (questionType === "mutiplechoice-multiple") { // For multiple choice (multiple) use checkbox buttons
+    var formattedCheckbox;
+    // Show possible answers
+    for (var i = 0; i < quiz.questions[currentQuestionIndex].possible_answers.length; i++) {
+      formattedCheckbox = HTMLcheckbox.replace("%data1%", i+1);
+      formattedCheckbox = formattedCheckbox.replace("%data2%", quiz.questions[currentQuestionIndex].possible_answers[i].caption);
+      $("#card-answers").append(formattedCheckbox);
+    }
+  }
+  else { // For true-false use radio buttons as well
+    $("#card-answers").append(HTMLfalse);
+    $("#card-answers").append(HTMLtrue);
+  }
 
 
 }
