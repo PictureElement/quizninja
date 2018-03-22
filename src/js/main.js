73f75d83f2412c014806fa6ef671f6e84984fdb2
Marios Sofokleous <marios.sofokleous@yandex.com>
2018-03-22 22:43:03 +0200

    Attach click event to the submit button. Add empty() function that clears the play area.

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,37 @@
+var quiz;
+var score = 0;
+var questionIndex = 0;
+
 // Populate play area
-function populate(quiz) {
-  
-  var score = 0;
-  var currentQuestionIndex = 0;
-  
+function populate() {
+
   // Get question type
-  var questionType = quiz.questions[currentQuestionIndex].question_type;
+  var questionType = quiz.questions[questionIndex].question_type;
 
   // Show image  
-  var formattedImage = HTMLimage.replace("%data%", quiz.questions[currentQuestionIndex].img); // Replace the %data% placeholder text
+  var formattedImage = HTMLimage.replace("%data%", quiz.questions[questionIndex].img); // Replace the %data% placeholder text
   $("#card-image").append(formattedImage); // Append image
 
   // Show question
-  var formattedQuestion = HTMLquestion.replace("%data%", quiz.questions[currentQuestionIndex].title); 
-  $("#card-body").prepend(formattedQuestion);
+  var formattedQuestion = HTMLquestion.replace("%data%", quiz.questions[questionIndex].title); 
+  $("#card-title").append(formattedQuestion);
 
   // Show possible answers
   if (questionType === "mutiplechoice-single") { // For multiple choice (single) use radio buttons
     var formattedRadio;
     // Show possible answers
-    for (var i = 0; i < quiz.questions[currentQuestionIndex].possible_answers.length; i++) {
+    for (var i = 0; i < quiz.questions[questionIndex].possible_answers.length; i++) {
       formattedRadio = HTMLradio.replace("%data1%", i+1);
-      formattedRadio = formattedRadio.replace("%data2%", quiz.questions[currentQuestionIndex].possible_answers[i].caption);
+      formattedRadio = formattedRadio.replace("%data2%", quiz.questions[questionIndex].possible_answers[i].caption);
       $("#card-answers").append(formattedRadio);
     }
   }
   else if (questionType === "mutiplechoice-multiple") { // For multiple choice (multiple) use checkbox buttons
     var formattedCheckbox;
     // Show possible answers
-    for (var i = 0; i < quiz.questions[currentQuestionIndex].possible_answers.length; i++) {
+    for (var i = 0; i < quiz.questions[questionIndex].possible_answers.length; i++) {
       formattedCheckbox = HTMLcheckbox.replace("%data1%", i+1);
-      formattedCheckbox = formattedCheckbox.replace("%data2%", quiz.questions[currentQuestionIndex].possible_answers[i].caption);
+      formattedCheckbox = formattedCheckbox.replace("%data2%", quiz.questions[questionIndex].possible_answers[i].caption);
       $("#card-answers").append(formattedCheckbox);
     }
   }
@@ -38,14 +39,23 @@ function populate(quiz) {
     $("#card-answers").append(HTMLfalse);
     $("#card-answers").append(HTMLtrue);
   }
-
-
 }
 
-// Quiz object
-var quiz;
+// Empty play area
+function empty() {
+  $("#card-image").empty();
+  $("#card-title").empty();
+  $("#card-answers").empty();
+}
 
 // Requests data from the server with an HTTP GET request
 $.getJSON("https://proto.io/en/jobs/candidate-questions/quiz.json", function(response) {
-  populate(response);
+  quiz = response;
+  populate();
+});
+
+$("#submit-btn").click(function() {
+  questionIndex++;
+  empty();
+  populate();
 });
