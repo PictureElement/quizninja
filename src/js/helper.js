e579caa38235f124efd1145cd7a0b5e3048b2fb0
Marios Sofokleous <marios.sofokleous@yandex.com>
2018-03-22 16:30:03 +0200

    Add HTML strings for multiple choice(multiple) checkbox buttons and True/false radio buttons.

--- src/js/helper.js
+++ src/js/helper.js
@@ -6,11 +6,17 @@ var HTMLcardImage = '<img class="img-fluid rounded" src="%data%" alt="placeholde
 var HTMLquestion = '<p class="p-3 m-0 text-center">%data%</p>';
 
 // Multiple choice (single)
-var HTMLmultipleChoiceSingleButton1 = '<input type="radio" autocomplete="off" name="options" id="option1" value="1"> %data%';
-var HTMLmultipleChoiceSingleButton2 = '<input type="radio" autocomplete="off" name="options" id="option2" value="2"> %data%';
-var HTMLmultipleChoiceSingleButton3 = '<input type="radio" autocomplete="off" name="options" id="option3" value="3"> %data%';
-var HTMLmultipleChoiceSingleButton4 = '<input type="radio" autocomplete="off" name="options" id="option4" value="4"> %data%';
+var HTMLradio1 = '<input type="radio" autocomplete="off" name="options" id="option1" value="1"> %data%';
+var HTMLradio2 = '<input type="radio" autocomplete="off" name="options" id="option2" value="2"> %data%';
+var HTMLradio3 = '<input type="radio" autocomplete="off" name="options" id="option3" value="3"> %data%';
+var HTMLradio4 = '<input type="radio" autocomplete="off" name="options" id="option4" value="4"> %data%';
 
 // Multiple choice (multiple)
+var HTMLcheckbox1 = '<input type="checkbox" autocomplete="off" name="options" id="option1" value="1"> %data%';
+var HTMLcheckbox2 = '<input type="checkbox" autocomplete="off" name="options" id="option2" value="2"> %data%';
+var HTMLcheckbox3 = '<input type="checkbox" autocomplete="off" name="options" id="option3" value="3"> %data%';
+var HTMLcheckbox4 = '<input type="checkbox" autocomplete="off" name="options" id="option4" value="4"> %data%';
 
 // True-false
+var HTMLradioTrue = '<input type="radio" autocomplete="off" name="options" id="option1" value="1"> True';
+var HTMLradioFalse = '<input type="radio" autocomplete="off" name="options" id="option2" value="2"> False';
