d8aa320701ae7900568c99cdd5c86db74804658a
Marios Sofokleous <marios.sofokleous@yandex.com>
2018-03-28 18:04:22 +0300

    Caching files with Service Worker using Cache API and Service Worker API.

--- src/sw.js
+++ src/sw.js
@@ -1,97 +1,123 @@
-// SERVICE WORKER SCRIPT
+/*
+Copyright 2016 Google Inc.
 
-var cacheName = 'quizninja-cache';
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-// Beware that index.html may also be requested as /
-var filesToCache = [
-  '/quizninja/',
-  '/quizninja/index.html',
-  '/quizninja/play.html',
+    http://www.apache.org/licenses/LICENSE-2.0
 
-  '/quizninja/js/main.js',
-  '/quizninja/js/helper.js',
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
-  '/quizninja/css/style.css',
-
-  '/quizninja/media/logo.svg',
-  '/quizninja/media/sfx-countdown.mp3',
-  '/quizninja/media/sfx-invalid-tone.mp3',
-  '/quizninja/media/sfx-katana.mp3',
-  '/quizninja/media/sfx-valid-tone.mp3',
-  '/quizninja/media/soundtrack-end.mp3',
-  '/quizninja/media/soundtrack-main.mp3'
+(function() {
+  'use strict';
 
   /*
-  'pages/offline.html',
-  'pages/404.html'
-  */
-];
+   * TODO 1 - CACHE THE APPLICATION SHELL
+   */
 
-/*
- * CACHE THE APP SHELL
- */
+  // Beware that index.html may also be requested as /
+  var filesToCache = [
+    '/quizninja/',
+    '/quizninja/index.html',
+    '/quizninja/pages/play.html',
+    '/quizninja/pages/offline.html',
+    '/quizninja/pages/404.html',
 
-// When the service worker is registered, an 'install' event is triggered the
-// first time the user visits the page
-self.addEventListener('install', function(event) {
-  console.log('Attempting to install service worker and cache static assets');
-  event.waitUntil(
-    caches.open(staticCacheName)
-    .then(function(cache) {
-      // Takes a list of URLs, then fetches them from the server and adds the
-      // response to the cache. This method is atomic and if any of the files
-      // fail, the entire cache step fails
-      return cache.addAll(filesToCache);
-    })
-  );
-});
+    '/quizninja/js/main.js',
+    '/quizninja/js/helper.js',
 
+    '/quizninja/style/main.css',
 
+    '/quizninja/media/logo.svg',
+    '/quizninja/media/sfx-countdown.mp3',
+    '/quizninja/media/sfx-invalid-tone.mp3',
+    '/quizninja/media/sfx-katana.mp3',
+    '/quizninja/media/sfx-valid-tone.mp3',
+    '/quizninja/media/soundtrack-end.mp3',
+    '/quizninja/media/soundtrack-main.mp3'
+  ];
 
+  var  staticCacheName = 'quizninja-cache';
 
+  // When the service worker is registered, an 'install' event is triggered the
+  // first time the user visits the page
+  self.addEventListener('install', function(event) {
+    console.log('Attempting to install service worker and cache static assets');
+    event.waitUntil(
+      caches.open(staticCacheName)
+      .then(function(cache) {
+        // Takes a list of URLs, then fetches them from the server and adds the
+        // response to the cache. This method is atomic and if any of the files
+        // fail, the entire cache step fails
+        return cache.addAll(filesToCache);
+      })
+    );
+  });
 
+  /*
+   * TODO 2 - INTERCEPT NETWORK REQUESTS
+   */
 
-
-// The 'activate' event is fired when the service worker starts up
-self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
-  // Logic to update the cache whenever any of the app shell files change
-  e.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
+  // The fetch event listener intercepts all requests. 
+  self.addEventListener('fetch', function(event) {
+    console.log('Fetch event for ', event.request.url);
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        if (response) {
+          console.log('Found ', event.request.url, ' in cache');
+          return response;
         }
-      }));
-    })
-  );
-  return self.clients.claim();
-});
+        // TODO 2.1 - ADD NETWORK RESPONSES TO THE CACHE
+        console.log('Network request for ', event.request.url);
+        return fetch(event.request).then(function(response) {
+          // TODO 2.2 - RESPOND WITH CUSTOM 404 PAGE
+          if (response.status === 404) {
+            return caches.match('pages/404.html');
+          }
+          return caches.open(staticCacheName).then(function(cache) {
+            if (event.request.url.indexOf('test') < 0) {
+              cache.put(event.request.url, response.clone());
+            }
+            return response;
+          });
+        });
+      }).catch(function(error) {
+        // TODO 2.3 - RESPOND WITH CUSTOM OFFLINE PAGE
+        console.log('Error, ', error);
+        return caches.match('pages/offline.html');
+      })
+    );
+  });
 
-/*
- * SERVE FILES FROM THE CACHE
- */
 
-// We've cached the app shell components, but we still need to load them from
-// the local cache.
-self.addEventListener('fetch', function(event) {
-  console.log('Fetch event for ', event.request.url);
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      if (response) {
-        console.log('Found ', event.request.url, ' in cache');
-        return response;
-      }
-      console.log('Network request for ', event.request.url);
-      return fetch(event.request)
+  /*
+   * TODO 3 - DELETE UNUSED CACHES
+   */
 
-      // TODO 4 - Add fetched files to the cache
+  // The 'activate' event is fired when the service worker starts up
+  self.addEventListener('activate', function(event) {
+    console.log('Activating new service worker...');
 
-    }).catch(function(error) {
+    var cacheWhitelist = [staticCacheName];
 
-      // TODO 6 - Respond with custom offline page
+    // Logic to update the cache whenever any of the app shell files change
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
 
-    })
-  );
-});
+})();
